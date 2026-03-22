151322f69d430afc3417ad39c2709f704cad5333
Anastasiia Yosypchuk <anastasiayosypcuk07@gmail.com>
2026-03-22 19:13:04 +0100

    HW7

--- HW7/main.py
+++ HW7/main.py
@@ -1,6 +1,6 @@
 from file_parser.file_parser import FileParse, CsvProcessor
 
 if __name__ == "__main__":
-    #FileParse.process_file()
+    FileParse.process_file()
     CsvProcessor.csv_word_count()
     CsvProcessor.csv_letter_count()
